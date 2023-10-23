5e5c0a3b3ea26bb26c9032454f080fdae6d3b4c9
Kyo <kyosenju1522@gmail.com>
2023-10-23 20:09:55 -0300

    ....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,21 +47,21 @@ export default function Home() {
           <div className='h-12 w-full p-2 my-2 flex justify-between'>
             <div className='flex space-x-2'>
               <a href="#" className='bg-black/20 rounded-full flex'>
-                <span className="material-symbols-outlined p-1 scale-75 relative left-0.5">arrow_back_ios</span>
+                <span className="material-symbols-outlined p-1 scale-75 pl-2">arrow_back_ios</span>
               </a>
               <a href="#" className='bg-black/20 rounded-full flex'>
-                <span className="material-symbols-outlined p-1 scale-75 relative left-0.5">arrow_forward_ios</span>
+                <span className="material-symbols-outlined p-1 scale-75 pr-2">arrow_forward_ios</span>
               </a>
             </div>
             <div className='flex space-x-2'>
               <a href="#" className='bg-black/20 rounded-full flex'>
-                <span className="material-symbols-outlined p-1 scale-75 relative left-0.5">notifications</span>
+                <span className="material-symbols-outlined p-1 scale-75 left-0.5">notifications</span>
               </a>
               <a href="#" className='bg-black/20 rounded-full flex'>
-                <span className="material-symbols-outlined p-1 scale-75 relative left-0.5">groups</span>
+                <span className="material-symbols-outlined p-1 scale-75 left-0.5">groups</span>
               </a>
               <a href="#" className='bg-black/20 rounded-full flex'>
-                <span className="material-symbols-outlined p-1 scale-75 relative left-0.5">person</span>
+                <span className="material-symbols-outlined p-1 scale-75 left-0.5">person</span>
               </a>
             </div>
           </div>
