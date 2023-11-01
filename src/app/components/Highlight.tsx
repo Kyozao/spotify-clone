963cc075fefe2f50eff022f8f54cf0e690864502
Kyo <kyosenju1522@gmail.com>
2023-11-01 17:20:47 -0300

    .

--- src/app/components/Highlight.tsx
+++ src/app/components/Highlight.tsx
@@ -1,6 +1,17 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Highlight(props: any) {
+  const [playHighlight, setPlayHighlight] = useState("Play");
+  const toggleButton = () => {
+    if (playHighlight === "Play") {
+      setPlayHighlight("Pause");
+    } else {
+      setPlayHighlight("Play");
+    }
+  };
+
   return (
     <div className="flex flex-col md:flex-row bg-black/50 py-12 gap-3 md:space-x-5 space-y-3 items-center px-5 mx-auto">
       <a className="">
@@ -17,8 +28,11 @@ export default function Highlight(props: any) {
         <h1 className="text-3xl md:text-6xl font-bold">{props.title}</h1>
         <span className="font-medium">{props.subtitle}</span>
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8">
-          <button className="bg-green-500 text-xl px-8 py-3 text-zinc-900 font-medium rounded-full flex justify-center items-center hover:scale-105 ring-green-500">
-            Play
+          <button
+            className="bg-green-500 text-xl px-8 py-3 text-zinc-900 font-medium rounded-full flex justify-center items-center hover:scale-105 ring-green-500"
+            onClick={toggleButton}
+          >
+            {playHighlight}
           </button>
           <button className="bg-transparent border border-zinc-400 text-xl px-8 py-3 text-zinc-50 font-medium rounded-full flex justify-center items-center hover:scale-105 hover:ring-1 ring-zinc-50">
             Follow
