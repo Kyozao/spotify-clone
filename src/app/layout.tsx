839bc44a978007e3201485c4b0bef347e13454fc
Kyo <kyosenju1522@gmail.com>
2023-11-01 16:11:03 -0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,29 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Spotify',
-  description: 'Listen to music.',
-}
+  title: "Spotify",
+  description: "Listen to music.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0"/>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0"
+        />
         <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
       </head>
       <body className="bg-zinc-950 text-zinc-50">{children}</body>
     </html>
-  )
+  );
 }
