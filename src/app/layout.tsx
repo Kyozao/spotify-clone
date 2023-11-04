c7878c299c8c10a4a0e662e008af217c61057c86
Kyo <kyosenju1522@gmail.com>
2023-11-04 17:10:17 -0300

    ;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "material-symbols";
-import SideBar from "./components/SideBar";
-import Footer from "./components/Footer";
-import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
